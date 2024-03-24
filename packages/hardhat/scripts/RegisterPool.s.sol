eb6dddb130698de5a1eb19073525acdf4b8e7c11
steve0xp <stevep0xp@gmail.com>
2024-03-24 15:06:13 -0700

    Fix RegisterPool.s.sol & reformat w/ utils

--- packages/hardhat/scripts/RegisterPool.s.sol
+++ packages/hardhat/scripts/RegisterPool.s.sol
@@ -3,10 +3,15 @@
 pragma solidity ^0.8.18;
 
 import { Script, console } from "forge-std/Script.sol";
-import { IVaultExtension, IERC20 } from "../contracts/interfaces/IVaultExtension.sol";
+import { IERC20 } from "../contracts/interfaces/IVaultExtension.sol";
 import { LiquidityManagement, IRateProvider, PoolHooks, TokenConfig, TokenType } from "../contracts/interfaces/VaultTypes.sol";
+import {TestAddresses} from "../test/utils/TestAddresses.sol";
+// import {TestAddresses} from  "packages/hardhat/test/utils/TestAddresses.sol";
 
 /**
+ * TODO - Steve, move this blob of comments to the PR once I make a dev branch, and a feature branch and a PR to fix the deployment script onto said dev branch.
+ * TODO - where I left off STEVE --> I gotta debug the dependencies / mappings cause I'm trying to use TestAddresses.sol (a new contract I made to make things cleaner) Ultimately though, I just need to test if IVault(vaultAddress).registerPool() works instead of calling IVaultExtension.registerPool()
+ * 
  * Register an already deployed pool on sepolia
  *
  * balancer docs
@@ -18,41 +23,46 @@ import { LiquidityManagement, IRateProvider, PoolHooks, TokenConfig, TokenType }
  * VaultTypes (TokenType, TokenConfig, IRateProvider)
  * https://github.com/balancer/balancer-v3-monorepo/blob/main/pkg/interfaces/contracts/vault/VaultTypes.sol
  */
-contract RegisterPool is Script {
-	IVaultExtension constant vaultExtension =
-		IVaultExtension(0x718e1176f01dDBb2409A77B2847B749c8dF4457f);
 
-	address sepoliaDAI = 0xB77EB1A70A96fDAAeB31DB1b42F2b8b5846b2613;
-	address sepoliaUSDC = 0x80D6d3946ed8A1Da4E226aa21CCdDc32bd127d1A;
+/**
+ * @title RegisterPool Script
+ * @author BUIDL GUIDL (placeholder)
+ * @notice The script registers a pool with the BalancerV3 Vault on sepolia 
+ * @dev This is in the early WIP stage, so we are working with already deployed pools for now. See PR # for context on related docs, code blobs, etc.
+ */
+contract RegisterPool is TestAddresses, Script {
+	// IVaultExtension constant vaultExtension =
+	// 	IVaultExtension(0x718e1176f01dDBb2409A77B2847B749c8dF4457f);
+
+	// address sepoliaDAI = 0xB77EB1A70A96fDAAeB31DB1b42F2b8b5846b2613;
+	// address sepoliaUSDC = 0x80D6d3946ed8A1Da4E226aa21CCdDc32bd127d1A;
 
 	function run() external {
 		uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
 		vm.startBroadcast(deployerPrivateKey);
 
-		/////////////////////////////
-		// args for registerPool  //
-		//////////////////////////
-		// 1. Address of pool to register
-		address pool = 0x37495BE0Af7B427Ddb9C504cE53617F9F04620aD;
-		// 2. An array of descriptors for the tokens the pool will manage.
-		TokenConfig[] memory tokenConfig = new TokenConfig[](2);
+		/// args for registerPool
+		address pool = address(0); // Address of pool to register NOTE - this pool is already registered, so you need to replace it or else the script will revert.
+
+		TokenConfig[] memory tokenConfig = new TokenConfig[](2); // An array of descriptors for the tokens the pool will manage.
+
+		// make sure to have proper token order (alphanumeric)
 		tokenConfig[0] = TokenConfig({
-			token: IERC20(sepoliaDAI),
-			tokenType: TokenType.STANDARD,
-			rateProvider: IRateProvider(address(0)),
-			yieldFeeExempt: false
-		});
-		tokenConfig[1] = TokenConfig({
 			token: IERC20(sepoliaUSDC),
 			tokenType: TokenType.STANDARD,
 			rateProvider: IRateProvider(address(0)),
 			yieldFeeExempt: false
 		});
-		// 3. The timestamp after which it is no longer possible to pause the pool
-		uint256 pauseWindowEndTime = 0;
-		// 4. Optional contract the Vault will allow to pause the pool
-		address pauseManager = address(0);
-		// 5. Flags indicating which hooks the pool supports
+		tokenConfig[1] = TokenConfig({
+			token: IERC20(sepoliaDAI),
+			tokenType: TokenType.STANDARD,
+			rateProvider: IRateProvider(address(0)),
+			yieldFeeExempt: false
+		});
+		uint256 pauseWindowEndTime = 0; // The timestamp after which it is no longer possible to pause the pool
+
+		address pauseManager = address(0); // Optional contract the Vault will allow to pause the pool
+
 		PoolHooks memory hookConfig = PoolHooks({
 			shouldCallBeforeInitialize: false,
 			shouldCallAfterInitialize: false,
@@ -62,17 +72,15 @@ contract RegisterPool is Script {
 			shouldCallAfterAddLiquidity: false,
 			shouldCallBeforeRemoveLiquidity: false,
 			shouldCallAfterRemoveLiquidity: false
-		});
-		// 6. Liquidity management flags with implemented methods
+		}); // Flags indicating which hooks the pool supports
+
 		LiquidityManagement memory liquidityManagement = LiquidityManagement({
 			supportsAddLiquidityCustom: false,
 			supportsRemoveLiquidityCustom: false
-		});
+		}); // Liquidity management flags with implemented methods
 
-		////////////////////////
-		// send register tx  //
-		//////////////////////
-		vaultExtension.registerPool(
+		/// send register tx 
+		vault.registerPool(
 			pool,
 			tokenConfig,
 			pauseWindowEndTime,
