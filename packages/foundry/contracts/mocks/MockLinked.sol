362c5d23cc779a05da19df0bfaf838791d0cb91c
spyros-zikos <zikossp@gmail.com>
2024-10-15 19:50:09 +0300

    fix typo

--- packages/foundry/contracts/mocks/MockLinked.sol
+++ packages/foundry/contracts/mocks/MockLinked.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.24;
 import { ERC20 } from "@openzeppelin-npm/token/ERC20/ERC20.sol";
 import { Ownable } from "@openzeppelin-npm/access/Ownable.sol";
 
-contract MockLinked_copy is ERC20, Ownable {
+contract MockLinked is ERC20, Ownable {
     // Mint the initial supply to the deployer
     constructor(string memory name, string memory symbol, uint256 initialSupply) ERC20(name, symbol) Ownable() {
         _mint(msg.sender, initialSupply);
